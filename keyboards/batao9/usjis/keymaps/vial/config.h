39ab1d5d620ae80213ecbe16bf5074462e1bc9a0
batao9 <batao.kai@gmail.com>
2025-10-13 00:10:42 +0900

    add usjis

---
+++ keyboards/batao9/usjis/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD3, 0x5B, 0xFD, 0xFD, 0x74, 0xBF, 0xD6, 0x69}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#define DYNAMIC_KEYMAP_MACRO_COUNT 0
+
+#define LAYER_STATE_8BIT
