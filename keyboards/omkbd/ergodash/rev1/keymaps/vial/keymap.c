27f518aa21dc7756128b3aaf501bee30c6ade220
batao9 <batao.kai@gmail.com>
2025-10-22 22:19:19 +0900

    update keymap

--- keyboards/omkbd/ergodash/rev1/keymaps/vial/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/vial/keymap.c
@@ -28,6 +28,8 @@ enum custom_keycodes {
 #define LT1_DEL LT(1, KC_DEL)
 #define LT2_DEL LT(2, KC_DEL)
 #define LT3_DEL LT(3, KC_DEL)
+#define CTL_SPC MT(MOD_RCTL, KC_SPC)
+#define ALT_SPC MT(MOD_RALT, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -48,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV ,                        KC_QUOT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_RALT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP  , KC_SLSH,
-    KC_LCTL, KC_LGUI, LT2_DEL, KC_LALT,          LT2_LG2, KC_SPC ,KC_LSFT,        KC_RSFT,KC_SPC , LT2_LG1,          KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        ALT_SPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP  , KC_SLSH,
+    KC_LCTL, KC_LGUI, LT2_DEL, KC_LALT,          LT2_LG2, KC_SPC ,KC_LSFT,        KC_RSFT,CTL_SPC, LT2_LG1,          KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Lower
